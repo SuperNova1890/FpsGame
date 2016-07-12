72e8a237ec77d952a83bdf7ae13a4cf49d1287de
Alan S <alstew13@hotmail.com>
2016-07-12 21:14:40 +0100

    Added Pause screen in game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject UIGamePaused;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (Time.timeScale > 0f) {
+				UIGamePaused.SetActive(true); // this brings up the pause UI
+				Time.timeScale = 0f; // this pauses the game action
+			} else {
+				Time.timeScale = 1f; // this unpauses the game action (ie. back to normal)
+				UIGamePaused.SetActive(false); // remove the pause UI
+			}
+		}
+	}
+}
